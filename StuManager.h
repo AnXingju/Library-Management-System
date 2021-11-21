784881e4e6a44de80db80fca685e3f37f71e0671
AnXingju <82820622+AnXingju@users.noreply.github.com>
2021-11-21 13:28:59 +0800

    Add files via upload

---
+++ StuManager.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct book
+{
+	char bnum[10];						//书籍编号
+	char bname[30];						//书名
+	char bauthor[20];					//作者
+	char bclassfy[10];					//类别编号
+	float bprice;						//价格
+	struct book* next;					//链表指针
+}BookInfo;
+
+BookInfo* CreateBooksList();			//创建链表
+void Insert(BookInfo* head);			//插入
+void Delete(BookInfo* head);			//删除
+void Print(BookInfo* head);				//浏览
+void Search(BookInfo* head);			//查询
+void Update(BookInfo* head);			//修改
+void Save(BookInfo* head);				//保存
+int menu();								//菜单
